12b2463d69fd60a7908e9967c55c37478e57e86b
Jace <jacebradford04@gmail.com>
2025-11-19 15:22:19 -0500

    update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import About from './pages/about';
-import { Route, Router, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
