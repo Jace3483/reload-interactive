99d84637ae359a509f158955f1a6a040f611f90c
Jace <jacebradford04@gmail.com>
2025-11-19 16:10:37 -0500

    Nopage

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Landing from './pages/landing';
 import About from './pages/about';
+import NoPage from './pages/nopage';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
@@ -9,6 +10,9 @@ function App() {
       <Routes>
         <Route path="/" element={<Landing />} />
         <Route path="/about" element={<About />} />
+
+        {/* Show NoPage for any invalid url */}
+        <Route path="*" element={<NoPage />} />
       </Routes>
     </Router>
   );
---
+++ src/pages/nopage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const nopage = () => {
+  return (
+    <div style={{ textAlign: "center", marginTop: "20vh", color: "#fff" }}>
+      <h1>404 - Page Not Found</h1>
+      <p>The page you're looking for doesn't exist.</p>
+    </div>
+  );
+};
+
+export default nopage;
